eca0bc0837c4841b02931c46d746c14934901b3d
sanwinmaung <sanwinmaung.dev@gmail.com>
2021-04-04 22:56:32 +0630

    Episode 2 - Initial Setup and Design

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>Voting System</title>
+    
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body class="font-sans bg-gray-background text-gray-900 text-sm">
+    <header class="flex items-center justify-between px-48 py-4">
+        <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
+        <div class="flex items-center ">
+            @if (Route::has('login'))
+            <div class="px-6 py-4">
+                @auth
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    
+                    <a href="route('logout')"
+                    onclick="event.preventDefault();
+                    this.closest('form').submit();">
+                    {{ __('Log out') }}
+                </a>
+            </form>
+            @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+            
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+            @endif
+            @endauth
+        </div>
+        @endif
+        
+        <a href="#">
+            <img src="https://www.gravatar.com/avatar/00000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+        </a>
+    </div>
+</header>
+
+<main class="container mx-auto max-w-custom flex">
+    <div class="w-70 mr-5">
+        Add idea form goes here. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, asperiores reprehenderit. Veniam sapiente et sed impedit, aliquid blanditiis dignissimos reprehenderit odio laboriosam porro quod vero, nostrum tempore quasi nulla excepturi dolor ducimus, dolorum fuga expedita dolores nisi laborum error ipsum!
+    </div>
+    <div class="w-175">
+        <nav class="flex items-center justify-between text-xs">
+            <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a></li>
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a></li>
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress (1)</a></li>
+            </ul>
+
+            <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10)</a></li>
+                <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a></li>
+            </ul>
+        </nav>
+
+        <div class="mt-8">
+            {{ $slot }}
+        </div>
+    </div>
+</main>
+</body>
+</html>
